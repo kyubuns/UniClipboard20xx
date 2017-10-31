94e8a2d977deadb9a74b3f1fbb8280cd6e056bb2
kyubuns <kyubuns@gmail.com>
2017-10-31 14:27:44 +0900

    iOS11だとテキストファイル扱いになるのを修正

--- Assets/UniClipboard/uniclipboard.mm
+++ Assets/UniClipboard/uniclipboard.mm
@@ -1,33 +1,30 @@
-#if ! __has_feature(objc_arc)
-#error This file must be compiled with ARC. Either turn on ARC for the project or use -fobjc-arc flag
-#endif
-
 NSString* UniClipboard_CreateNSString(const char* string)
 {
-	return [NSString stringWithUTF8String: string ? string : ""];
+  return [NSString stringWithUTF8String: string ? string : ""];
 }
 
 char* UniClipboard_MakeStringCopy(const char* string)
 {
-	if (string == NULL) return NULL;
-	char* res = (char*)malloc(strlen(string) + 1);
-	strcpy(res, string);
-	return res;
+  if (string == NULL) return NULL;
+  char* res = (char*)malloc(strlen(string) + 1);
+  strcpy(res, string);
+  return res;
 }
 
 extern "C"
 {
-	void _SetClipboard(const char* text)
-	{
-		NSString *nsstringText = UniClipboard_CreateNSString(text);
-		UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
-		[pasteboard setValue:nsstringText forPasteboardType:@"public.text"];
-	}
+  void _SetClipboard(const char* text)
+  {
+    NSString *nsstringText = UniClipboard_CreateNSString(text);
+    UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+    pasteboard.string = nsstringText;
+  }
 
-	char *_GetClipboard()
-	{
-		UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
-		NSString *string = [pasteboard valueForPasteboardType:@"public.text"];
-		return UniClipboard_MakeStringCopy([string UTF8String]);
-	}
+  char *_GetClipboard()
+  {
+    UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+    NSString *string = pasteboard.string;
+    return UniClipboard_MakeStringCopy([string UTF8String]);
+  }
 }
+
